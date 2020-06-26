c922452511a30af50b37d2cfe839630768d4db7e
Ruslan <ruslantrade2019@gmail.com>
2020-06-26 16:36:38 +0700

    first commit add desktop version

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  $('.slider').slick({
+    slidesToShow:1,
+    slidesToScroll:1,
+    arrows:true,
+    dots:true,
+  });
+
+  $('.services-detail__slider').slick({
+    slidesToShow:1,
+    slidesToScroll:1,
+    dots:false,
+    arrows:true,
+  });
+
+  $('.reviews__slider').slick({
+    slidesToShow:3,
+    slidesToScroll:1,
+    dots:false,
+    arrows:true,
+
+    
+  })
+})
