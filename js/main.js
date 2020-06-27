c66d2f77df83ed388d348a38173316697664b0d0
Ruslan <ruslantrade2019@gmail.com>
2020-06-27 21:48:38 +0700

    ready mobile first(need mobile menu!)

--- js/main.js
+++ js/main.js
@@ -1,23 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   $('.slider').slick({
-    slidesToShow:1,
-    slidesToScroll:1,
-    arrows:true,
-    dots:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
   });
 
   $('.services-detail__slider').slick({
-    slidesToShow:1,
-    slidesToScroll:1,
-    dots:false,
-    arrows:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    responsive: [{
+      breakpoint: 544,
+      settings: {
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+      }
+    }, ]
   });
 
+
   $('.reviews__slider').slick({
-    slidesToShow:3,
-    slidesToScroll:1,
-    dots:false,
-    arrows:true,
-  })
-})
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          dots: true,
+        }
+      },
+    ]
+  });
+
+
+
+});
